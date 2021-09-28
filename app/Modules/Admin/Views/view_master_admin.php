7267ba7fda708bd37acc80a8c280e14388f6a0c6
Rafho <44487637+rafhoerdeft@users.noreply.github.com>
2021-09-28 14:23:25 +0700

    Delete app/Modules/Admin directory
    delete admin

--- app/Modules/Admin/Views/view_master_admin.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    echo view('komponen/header',$header);
-    echo view('admin/menu',$menu);
-    if(isset($konten))echo view($konten, $cont);
-    echo view('komponen/footer',$footer);
-?>
